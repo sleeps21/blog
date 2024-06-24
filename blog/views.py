5dc6aeaa56b25debd23913101908498922353c5f
sleeps21 <j.sleeps47@gmail.com>
2024-06-24 21:59:36 +0200

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import DetailView, ListView  # new
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):  # new
+    model = Post
+    template_name = "post_detail.html"
